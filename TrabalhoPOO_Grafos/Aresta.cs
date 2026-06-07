44b657f5a8bfb12cf2c18554a9eba8042243a998
felipe <felippelopes05@gmail.com>
2026-06-07 12:50:11 -0300

    classe aresta

--- TrabalhoPOO_Grafos/Aresta.cs
+++ TrabalhoPOO_Grafos/Aresta.cs
@@ -7,6 +7,27 @@ namespace TrabalhoPOO_Grafos
 {
     public class Aresta
     {
-        
+        private int origem;
+        private int destino;
+        private int peso;
+
+        public Aresta(int origem, int destino, int peso)
+        {
+            this.origem = origem;
+            this.destino = destino;
+            this.peso = peso;
+        }
+        public int GetOrigem()
+        { 
+            return this.origem;
+        }
+        public int GetDestino() 
+        { 
+            return rhis.destino; 
+        }
+        public int getPeso() 
+        { 
+            return this.peso; 
+        }
     }
 }
